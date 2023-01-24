5a142144caebb69de4db92502d11dc9443fc86d3
Grimnebuulen <mr.trader92@gmail.com>
2023-01-24 20:24:21 +0200

    add home work

--- dj_hw/settings.py
+++ dj_hw/settings.py
@@ -73,10 +73,14 @@ WSGI_APPLICATION = 'dj_hw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "DATABASE_NAME",
+        "HOST": "POSTGRESQL_HOST",
+        "PORT": "POSTGRESQL_PORT",
+        "USER": "POSTGRESQL_USER",
+        "PASSWORD": "POSTGRESQL_PASSWORD",
     }
 }
 
