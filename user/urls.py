08aa5419eb82ba22a4647ac70b6814fb746f2ecc
Grimnebuulen <mr.trader92@gmail.com>
2023-02-24 14:13:49 +0200

    first commit

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from user.views import signin, signup, user_logout
+
+
+urlpatterns = [
+    path('signin/', signin, name='signin'),
+    path('signup/', signup, name='signup'),
+    path('logout/', user_logout, name='logout'),
+]
