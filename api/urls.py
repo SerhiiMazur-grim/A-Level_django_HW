4ddd218b3d4082ce3052caebda535229c7f4f3ec
Grimnebuulen <mr.trader92@gmail.com>
2023-02-28 11:19:06 +0200

    added user api

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,11 @@ from .views import (
     ArticleCreateAPIView,
     ArticleUpdateAPIView,
     ArticleDeleteAPIView,
+    UserListAPIView,
+    UserDetailView,
+    UserCreateAPIView,
+    UserUpdateAPIView,
+    UserDeleteView,
 )
 
 
@@ -15,4 +20,10 @@ urlpatterns = [
     path('api/articles/create/', ArticleCreateAPIView.as_view(), name='article_create'),
     path('api/articles/<int:pk>/update/', ArticleUpdateAPIView.as_view(), name='article_update'),
     path('api/articles/<int:pk>/delete/', ArticleDeleteAPIView.as_view(), name='article_delete'),
+    
+    path('api/users/', UserListAPIView.as_view(), name='user_list'),
+    path('api/users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('api/users/create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('api/users/<int:pk>/update/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('api/users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import (ListAPIView,
                                     RetrieveAPIView,
                                     CreateAPIView,
@@ -5,6 +8,7 @@ from rest_framework.generics import (ListAPIView,
                                     DestroyAPIView)
 
 from blog.serializers import ArticleSerializer
+from user.serializers import UserSerializer
 from blog.models import Article
 
 
@@ -31,3 +35,38 @@ class ArticleUpdateAPIView(UpdateAPIView):
 class ArticleDeleteAPIView(DestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        password = make_password(self.request.data['password'])
+        serializer.save(password=password)
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_update(self, serializer):
+        password = serializer.validated_data.get('password')
+        if password is not None:
+            serializer.validated_data['password'] = make_password(password)
+        serializer.save()
+
+
+class UserDeleteView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
